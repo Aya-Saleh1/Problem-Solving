728a6668cc1acbc7c79f5b909655d031efe1cc57
Aya <ayasaleh1105@gmail.com>
2025-11-14 09:54:02 +0200

    Update Mo_algo_less_time.cpp

--- Mo_algo_less_time.cpp
+++ Mo_algo_less_time.cpp
@@ -1,140 +1,125 @@
 // better time complexity then original templet 
 // https://codeforces.com/blog/entry/61203 -> blog about it 
+// if you have strict time -> use this 
 #include <bits/stdc++.h>
 using namespace std;
-using namespace chrono;
+#define int long long
+#define ll long long
+#define endl '\n'
+#define FAST ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+const int MOD = 1e9 + 7;
+const int INF = 1e18 + 7;
+const int MOD2 = 1e9 + 9;
 
-#ifdef DEBUG
-	//#define LOCAL_INPUT_FILE
-#else
-	//#define USE_FILE_IO
+void fileIO() {
+#ifndef ONLINE_JUDGE
+    freopen("saleh.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
 #endif
+}
 
-#ifdef USE_FILE_IO
-	#define INPUT_FILE "input.txt"
-	#define OUTPUT_FILE "output.txt"
-	#define cin ____cin
-	#define cout ____cout
-	ifstream cin(INPUT_FILE);
-	ofstream cout(OUTPUT_FILE);
-#else
-	#ifdef LOCAL_INPUT_FILE
-		#define cin ____cin
-		ifstream cin("input.txt");
-	#endif
-#endif
-
-const int infinity = (int)1e9 + 42;
-const int64_t llInfinity = (int64_t)1e18 + 256;
-const int module = (int)1e9 + 7; 
+const int N = 2e5 + 5;
+vector<int> blk;
+const int M = 1e6 + 5;
+int sq = sqrtl(N) + 1;
+vector<int> v;
+vector<int> ans;
+int res;
+const int infinity = (int) 1e9 + 42;
+const int64_t llInfinity = (int64_t) 1e18 + 256;
+const int module = (int) 1e9 + 7;
 const long double eps = 1e-8;
-
+using namespace chrono;
 mt19937_64 randGen(system_clock().now().time_since_epoch().count());
 
 inline void raiseError(string errorCode) {
-	cerr << "Error : " << errorCode << endl;
-	exit(42);
+    cerr << "Error : " << errorCode << endl;
+    exit(42);
 }
 
 inline int64_t gilbertOrder(int x, int y, int pow, int rotate) {
-	if (pow == 0) {
-		return 0;
-	}
-	int hpow = 1 << (pow-1);
-	int seg = (x < hpow) ? (
-		(y < hpow) ? 0 : 3
-	) : (
-		(y < hpow) ? 1 : 2
-	);
-	seg = (seg + rotate) & 3;
-	const int rotateDelta[4] = {3, 0, 0, 1};
-	int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
-	int nrot = (rotate + rotateDelta[seg]) & 3;
-	int64_t subSquareSize = int64_t(1) << (2*pow - 2);
-	int64_t ans = seg * subSquareSize;
-	int64_t add = gilbertOrder(nx, ny, pow-1, nrot);
-	ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
-	return ans;
+    if (pow == 0) return 0;
+    int hpow = 1 << (pow - 1);
+    int seg = (x < hpow) ? ((y < hpow) ? 0 : 3) : ((y < hpow) ? 1 : 2);
+    seg = (seg + rotate) & 3;
+    static const int rotateDelta[4] = {3, 0, 0, 1};
+    int nx = x & (hpow - 1), ny = y & (hpow - 1);
+    int nrot = (rotate + rotateDelta[seg]) & 3;
+    int64_t sub = 1LL << (2 * pow - 2);
+    int64_t res = seg * sub;
+    int64_t add = gilbertOrder(nx, ny, pow - 1, nrot);
+    return res + ((seg == 1 || seg == 2) ? add : (sub - add - 1));
 }
 
-struct Query {
-	int l, r, idx;
-	int64_t ord;
-	
-	inline void calcOrder() {
-		ord = gilbertOrder(l, r, 21, 0);
-	}
+struct qu {
+    int l, r, idx;
+    int64_t ord;
+
+    inline void calcOrder() {
+        ord = gilbertOrder(l, r, 21, 0);
+    }
 };
 
-inline bool operator<(const Query &a, const Query &b) {
-	return a.ord < b.ord;
+vector<qu> que;
+vector<int> vis;
+
+inline bool operator<(const qu &a, const qu &b) {
+    return a.ord < b.ord;
 }
 
-signed main() {
-	#ifndef USE_FILE_IO
-		ios_base::sync_with_stdio(false);
-	#endif
-	
-	mt19937 rnd(42);
-	
-	int n, m, k; cin >> n >> m; k = rnd() % 1048576;
-	vector<int> p(n+1);
-	for (int i = 0; i < n; i++) {
-		int val = rnd() % 1048576;
-		p[i+1] = p[i] ^ val;
-	}
-	
-	vector<Query> qry(m);
-	for (int i = 0; i < m; i++) {
-		int l = rnd() % n + 1, r = rnd() % n + 1;
-		if (l > r) {
-			swap(l, r);
-		}
-		qry[i].l = l; qry[i].r = r;
-		qry[i].idx = i;
-		qry[i].calcOrder();
-	}
-	
-	int64_t ans = 0;
-	vector<int64_t> res(m);
-	vector<int64_t> cnt((int)2e6, 0);
-	sort(qry.begin(), qry.end());
-	int l = 0, r = 1;
-	ans = (p[1] == k);
-	cnt[p[0]]++; cnt[p[1]]++;
-	
-	for (Query q: qry) {
-		q.l--;
-		while (l > q.l) {
-			l--;
-			ans += cnt[p[l] ^ k];
-			cnt[p[l]]++;
-		}
-		while (r < q.r) {
-			r++;
-			ans += cnt[p[r] ^ k];
-			cnt[p[r]]++;
-		}
-		while (l < q.l) {
-			cnt[p[l]]--;
-			ans -= cnt[p[l] ^ k];
-			l++;
-		}
-		while (r > q.r) {
-			cnt[p[r]]--;
-			ans -= cnt[p[r] ^ k];
-			r--;
-		}
-		res[q.idx] = ans;
-	}
-	
-	uint64_t rhsh = 0;
-	for (int i = 0; i < m; i++) {
-		rhsh *= (uint64_t)1e9 + 7;
-		rhsh += (uint64_t)res[i];
-	}
-	cout << rhsh << "\n";
-	
-	return 0;
+
+void add(int idx) {
+}
+
+void remove_(int idx) {
+}
+
+void mo_algo() {
+    sort(que.begin(), que.end());
+    int L = 0, R = -1;
+
+    for (auto &q: que) {
+        while (L > q.l) add(--L);
+        while (R < q.r) add(++R);
+        while (L < q.l) remove_(L++);
+        while (R > q.r) remove_(R--);
+
+        ans[q.idx] = res;
+    }
+}
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    v.resize(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    que.resize(q);
+    ans.resize(q);
+    vis = vector<int>(M);
+    res = 0;
+    for (int i = 0; i < q; i++) {
+        cin >> que[i].l >> que[i].r;
+        que[i].l--, que[i].r--;
+        que[i].idx = i;
+    }
+    for (int i = 0; i < q; i++)
+        que[i].calcOrder();
+    mo_algo();
+    for (int i = 0; i < q; i++)
+        cout << ans[i] << '\n';
+}
+
+
+int32_t main() {
+    FAST
+    fileIO();
+    int t = 1;
+    // cout << fixed << setprecision(8);
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
 }
 
