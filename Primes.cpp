bdf08d06ff6804e13d460a4eacb2ed4da96b6889
Aya <ayasaleh1105@gmail.com>
2026-01-11 23:49:43 +0200

    Create Primes.cpp

---
+++ Primes.cpp
@@ -0,0 +1,24 @@
+// Find primes in range
+// Sometimes we need to find all prime numbers in a range [L,R] of small size (e.g. R - L + 1 \approx 1e7 ), where  R can be very large (e.g.1e12 ).
+O( (R - L + 1 )loglog (R) + sqrt(R) loglog (sqrt(R)) )
+vector<char> segmentedSieve(long long L, long long R) {
+    // generate all primes up to sqrt(R)
+    long long lim = sqrt(R);
+    vector<char> mark(lim + 1, false);
+    vector<long long> primes;
+    for (long long i = 2; i <= lim; ++i) {
+        if (!mark[i]) {
+            primes.emplace_back(i);
+            for (long long j = i * i; j <= lim; j += i)
+                mark[j] = true;
+        }
+    }
+
+    vector<char> isPrime(R - L + 1, true);
+    for (long long i : primes)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
